ca321c10813b807b964cdd79e0c7faf18d83cc46
shiyuanba <3065310874@qq.com>
2023-09-25 16:06:00 +0800

    添加员工功能已初步完成

--- src/main/java/com/hjc/reggie/controller/EmployeeController.java
+++ src/main/java/com/hjc/reggie/controller/EmployeeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -24,26 +25,27 @@ public class EmployeeController {
 
     /**
      * 员工登录
+     *
      * @param request
      * @param employee
      * @return
      */
     @PostMapping("/login")
-    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
         //md5加密
         String password = employee.getPassword();
         password = DigestUtils.md5DigestAsHex(password.getBytes());
 
         //判断用户是否存在
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
         Employee emp = service.getOne(queryWrapper);
         if (emp == null) {
             return R.error("用户不存在");
         }
 
         //查看密码是否正确
-        if (!emp.getPassword().equals(password)){
+        if (!emp.getPassword().equals(password)) {
             return R.error("密码错误");
         }
 
@@ -51,18 +53,40 @@ public class EmployeeController {
         if (emp.getStatus() == 0) {
             return R.error("用户被锁定");
         }
-        request.getSession().setAttribute("employee",emp.getId());
+        request.getSession().setAttribute("employee", emp.getId());
         return R.success(emp);
     }
 
     /**
      * 员工退出
+     *
      * @param request
      * @return
      */
     @PostMapping("/logout")
-    public R<String> logout(HttpServletRequest request){
+    public R<String> logout(HttpServletRequest request) {
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
     }
+
+    @PostMapping
+    public R<String> save(HttpServletRequest request,@RequestBody Employee employee) {
+        log.info("员工信息：{}", employee.toString());
+        //
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //获得当前用户的id
+        Long empid = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empid);
+        employee.setUpdateUser(empid);
+        //
+        boolean result = service.save(employee);
+        if (result){
+            return R.success("添加成功");
+        }
+        return R.error("添加失败");
+    }
+
 }
