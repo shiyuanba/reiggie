e82588e7d9ba65ef5b057481c9d2d6ebb83df04b
shiyuanba <3065310874@qq.com>
2023-10-03 20:21:31 +0800

    文件上传和下载功能已完成

---
+++ src/main/java/com/hjc/reggie/controller/CommonController.java
@@ -0,0 +1,96 @@
+package com.hjc.reggie.controller;
+
+import com.hjc.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.UUID;
+
+/**
+ * 文件上传和下载
+ */
+@RestController
+@RequestMapping("/common")
+@Slf4j
+public class CommonController {
+    //从yml文件中注入值
+    @Value("${reggie.path}")
+    private String basePath;
+
+    /**
+     * 文件上传
+     *
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
+    public R<String> upload(MultipartFile file) {
+
+        //使用UUID生成文件名，防止文件覆盖
+        String fileName = UUID.randomUUID().toString();
+
+        //获取原始文件名
+        String originalFilename = file.getOriginalFilename();
+        //截取后缀名
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        fileName = fileName + suffix;
+
+        File dir = new File(basePath);
+
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        try {
+            file.transferTo(new File(basePath + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return R.success(fileName);
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param response
+     * @param name
+     */
+    @GetMapping("/download")
+    public void download(HttpServletResponse response, String name) {
+        ServletOutputStream outputStream = null;
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(new File(basePath+name));
+            outputStream = response.getOutputStream();
+            response.setContentType("img/jpeg");
+            byte[] bytes = new byte[1024];
+            int len = 0;
+            while ((len = fis.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, len);
+                outputStream.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
