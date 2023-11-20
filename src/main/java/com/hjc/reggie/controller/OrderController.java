d957dafba9ef1323ce19c81cf52219809bc86c1a
shiyuanba <3065310874@qq.com>
2023-11-20 20:06:36 +0800

    订单信息分页查询和派送功能已完成

--- src/main/java/com/hjc/reggie/controller/OrderController.java
+++ src/main/java/com/hjc/reggie/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.hjc.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hjc.reggie.common.R;
+import com.hjc.reggie.dto.OrdersDto;
 import com.hjc.reggie.entity.Orders;
 import com.hjc.reggie.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
@@ -26,7 +28,7 @@ public class OrderController {
      */
     @PostMapping("/submit")
     public R<String> save(@RequestBody Orders orders) {
-        log.info("用户下单：{]",orders.toString());
+        log.info("用户下单：{]", orders.toString());
         orderService.submit(orders);
         return R.success("支付成功！");
     }
@@ -34,25 +36,39 @@ public class OrderController {
 
     /**
      * 订单信息分页查询
+     *
      * @param page
      * @param pageSize
      * @return
      */
     @GetMapping("/page")
-    public R<Page> page(int page, int pageSize){
-        Page<Orders> orderPage = new com.baomidou.mybatisplus.extension.plugins.pagination.Page<>(page,pageSize);
+    public R<Page> page(int page, int pageSize) {
+        Page<Orders> orderPage = new com.baomidou.mybatisplus.extension.plugins.pagination.Page<>(page, pageSize);
         orderService.page(orderPage);
 
         return R.success(orderPage);
     }
 
     /**
-     *
+     * 修改订单信息
      * @return
      */
     @PutMapping
-    public R<String> setStatus(@RequestBody Orders order){
+    public R<String> setStatus(@RequestBody Orders order) {
         orderService.updateById(order);
         return R.success("修改成功");
+    }
+
+    /**
+     * 返回订单信息
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("userPage")
+    public R<Page<Orders>> userPage(int page, int pageSize) {
+        Page<Orders> ordersPage = new Page<>(page, pageSize);
+        orderService.page(ordersPage);
+        return R.success(ordersPage);
     }
 }
